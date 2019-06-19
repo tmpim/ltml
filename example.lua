24e85059dc06b04ea0902f0596c1cdc0aed0aed3
Justyn <egot@istic.pw>
2019-06-18 20:47:47 -0500

    Explain utils and data in the example

--- example.lua
+++ example.lua
@@ -2,6 +2,9 @@ local ltml = require("ltml")
 local utils = require("ltml.utils")
 
 -- Load the an .ltml.lua template from a specific path.
+-- It is highly recommended that you do not use the 
+-- ltml.utils module if you can load the template yourself.
+-- This function is simply used to shorten the example.
 local template = utils.loadTemplateFromFile("example/example.ltml.lua")
 
 -- Create the table which we will supply in the environment.
@@ -14,6 +17,11 @@ local data = {
 -- only using it once. However, compile optimizes away
 -- a compilation step and makes templates run faster.
 local htmlTemplate = ltml.compile(template)
+
+-- Execute the template, returning a tree which we can
+-- pass off to the renderer. We supply data as `data`
+-- within the env to ensure we don't overwrite any important
+-- variables provided by the default sandbox.
 local htmlTree = htmlTemplate({ data = data })
 
 -- `ltml.render` simply returns the raw rendered HTML as a string.
